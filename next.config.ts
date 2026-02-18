2226886fa4210b903e209b77bd9d636dda4d364e
Ricky <vianeyricky@gmail.com>
2026-02-18 11:22:11 +0700

    edit next js

--- next.config.ts
+++ next.config.ts
@@ -18,32 +18,6 @@ const nextConfig: NextConfig = {
     ],
   },
 
-  // --- TAMBAHAN BARU: SECURITY HEADERS ---
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN", // Mencegah web Anda dimasukkan ke iframe web penipu
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff", // Mencegah browser menebak tipe file (mencegah XSS)
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin", // Melindungi data asal pengunjung
-          },
-          {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=(), browsing-topics=()", // Mematikan akses fitur yang tidak dipakai
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
